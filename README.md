7ed68f42f296c522c3ffc6f6ac0259508cbce57c
Dan Clark <dan@sammy-davis.(none)>
2012-10-23 23:08:01 -0400

    Now have a grasp of how the response entity is accessed.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-An attempt to roll my own OAuth client library in Java.
+An attempt to roll my own OAuth client library in Java. The goals of this are the following.
+
+- Become aquainted with Java again.
+- Introduction to git and GitHub
+- Introduction to OAuth 1.0
+- Dive deeper into HTTP via the HTTPComponents (Apache) library
+
+-------------------------------------------------------------------------------
+Changelog
+-------------------------------------------------------------------------------
+
+*2012-10-23*:Became aware of how to parse the response entity to retrieve the
+    http payload.
+*2012-10-21*: Initial build of client, request, and response handling.
+    Everthing makes sense except for the response.
--- src/basicOAuth/BasicOAuth.java
+++ src/basicOAuth/BasicOAuth.java
@@ -1,6 +1,9 @@
 package basicOAuth;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -8,6 +11,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 
 public class BasicOAuth {
@@ -17,11 +21,20 @@ public class BasicOAuth {
 	 */
 	public static void test() {
 		HttpClient http_client = new DefaultHttpClient();
-		HttpGet http_get = new HttpGet("http://google.com");
+		HttpGet http_get = new HttpGet("http://danclark.h0stname.net");
 		try {
 			HttpResponse response = http_client.execute(http_get);
 			HttpEntity entity = response.getEntity();
-			System.out.println(entity);
+			System.out.println(response.getStatusLine());
+			InputStream instream = entity.getContent();
+			Reader r = new InputStreamReader(instream, "US-ASCII");
+			System.out.print(entity);
+			int intch;
+			while ((intch = r.read()) != -1) {
+				char ch = (char) intch;
+				System.out.print(ch);
+			}
+			//System.out.println(EntityUtils.toString(entity));
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
